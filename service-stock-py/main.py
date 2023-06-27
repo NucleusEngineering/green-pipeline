a77087fbe858d4d8b7eefd7da01dd947602db245
FabianoAO <fabiano-abreu-ott@gmail.com>
2023-06-27 09:47:14 +0100

    new finance API

--- service-stock-py/main.py
+++ service-stock-py/main.py
@@ -1,10 +1,11 @@
 import json
+import requests
 
 from flask import Flask
 from flask import request
 from flask import Response
 
-import yfinance as yf
+#import yfinance as yf
 
 app = Flask(__name__)
 
@@ -16,12 +17,24 @@ def simulate_stock_price(methods=['GET', 'POST']):
     if stock_symbol is None or stock_symbol == "":
        return Response("URL parameter stock not provided", status=200)
 
-    stock = yf.Ticker(stock_symbol)
-    stockprice = stock.info['currentPrice']
-    print (stockprice)
+    url = "https://yfapi.net/v6/finance/quote"
+    querystring = {}
+    symbols = stock_symbol
+    for information in ["symbols"]:      
+        querystring[information] = eval(information)  
+
+    headers = {
+    'x-api-key': "xKqFyxmTgdauCo4S1LRdD5adrSlyFK9J9if4VFfZ"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response_data =  response.json()
+    stock_price = response_data['quoteResponse']['result'][0]['regularMarketPrice']
+
+    print(stock_price)
     
-    return str(stockprice)
+    return str(stock_price)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
+            
