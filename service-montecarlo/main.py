cf24252313f6183f1aa3cbb787a4b80805f6e506
Michael Dietz <michael.dtz@gmail.com>
2023-06-29 10:52:29 +0200

    Updates

--- service-montecarlo/main.py
+++ service-montecarlo/main.py
@@ -20,20 +20,34 @@ def montecarlo_simulation(methods=['GET', 'POST']):
   bandwidth   = float(bandwidth)
   results = {}
  
-  def montecarlo_simulation_calc(iterations, initial_stock_value, bandwidth): 
-
+  def montecarlo_simulation_calc(iterations, initial_stock_value):
+ 
     # Generate a random number generator.
     rng = np.random.default_rng()
 
-    # Calculate the new stock price for each iteration.
-    stock_prices = [initial_stock_value * (1 + rng.random() * bandwidth) for _ in range(iterations)]
+    # Create a list to store the simulated stock prices.
+    stock_prices = []
 
-    # Calc mean & std of list of simulated stock prices.
+    # For each iteration, generate a random stock price.
+    for _ in range(iterations):
+
+      # Generate a random number between 0 and 1 and calculate with bandwidth.
+      random_number = (rng.random() - 0.5) * bandwidth
+
+      # Calculate the new stock price.
+      new_stock_price = initial_stock_value * (1 + random_number)
+
+      # Add the new stock price to the list.
+      stock_prices.append(new_stock_price)
+   
+  
+    # Calc  mean & std of list of simulated stock prices.
     results = {
-        "value_mean": np.mean(stock_prices),
-        "value_std": np.std(stock_prices)
-    }
+      "value_mean": np.mean(stock_prices),
+      "value_std": np.std(stock_prices)
+    }  
 
+    # Return the results
     return results
   
   return montecarlo_simulation_calc(iterations, value_float)  
