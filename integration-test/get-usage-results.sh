121c39579125bb4eb246d207e68325f6ac29287d
Michael Dietz <michael.dtz@gmail.com>
2023-06-29 10:36:13 +0200

    Los gehts

--- integration-test/get-usage-results.sh
+++ integration-test/get-usage-results.sh
@@ -1,8 +1,9 @@
-SELECT 
-"The integration tests on commit " || commitId ||
+bq query --nouse_legacy_sql --parameter commitId:STRING:$SHORT_SHA \
+'SELECT "The integration tests on commit " || commitId ||
 " was executed with a usage of "  || cpu_amount || " CPU-Seconds " ||
 " and a usage of " || memory_amount || " GB-Seconds"
 
 FROM 
 `greenops-demo-env.greenpipeline_metering.usage_metering_per_commitid`
 WHERE commitId = "@commitId"
+'
