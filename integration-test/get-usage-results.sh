4f7fb017489cd5fb708c9ba7d6186c2b6aa972ed
midietz <midietz@google.com>
2024-09-17 14:20:19 +0000

    improve usage results

--- integration-test/get-usage-results.sh
+++ integration-test/get-usage-results.sh
@@ -1,10 +1,23 @@
+set -x #echo on
+
 bq query --nouse_legacy_sql --parameter commitId:STRING:$SHORT_SHA \
 'SELECT "The integration tests on commit " || commitId ||
-" was executed with a usage of "  || cpu_amount || " CPU-Seconds " ||
-" and a usage of " || memory_amount || " GB-Seconds"
+" was executed with a usage of "  || ROUND(cpu_amount,2) || " CPU-Seconds " ||
+" and a usage of " || ROUND(memory_amount,2) || " GB-Seconds"
 
 FROM 
 `greenops-demo-env.greenpipeline_metering.usage_metering_per_commitid`
 WHERE commitId = "@commitId"
 '
+
+bq query --nouse_legacy_sql --parameter commitId:STRING:$SHORT_SHA \
+'
+SELECT 
+" Over the previous commit, the CPU usage improved by "  || ROUND(cpu_improvement,2) || " CPU-Seconds " 
+" and the memory usage improved by "  || ROUND(mem_improvement,2) || " CPU-Seconds " 
+  FROM `greenops-demo-env.greenpipeline_metering.commit_usage_impact` 
+WHERE commitId = "@commitId"
+'
+
+
  
