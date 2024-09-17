b3357fa78b804a6f7643d430fb3dfef8087277e3
midietz <midietz@google.com>
2024-09-17 20:42:54 +0000

    update usage result getter

--- integration-test/get-usage-results.sh
+++ integration-test/get-usage-results.sh
@@ -1,23 +1,19 @@
 set -x #echo on
 
-bq query --nouse_legacy_sql --parameter commitId:STRING:$SHORT_SHA \
-'SELECT "The integration tests on commit " || commitId ||
+bq query --use_legacy_sql=false --parameter=commitId:STRING:$SHORT_SHA '
+SELECT "The integration tests on commit " || commitId ||
 " was executed with a usage of "  || ROUND(cpu_amount,2) || " CPU-Seconds " ||
 " and a usage of " || ROUND(memory_amount,2) || " GB-Seconds"
-
-FROM 
-`greenops-demo-env.greenpipeline_metering.usage_metering_per_commitid`
+FROM `greenops-demo-env.greenpipeline_metering.usage_metering_per_commitid`
 WHERE commitId = "@commitId"
 '
 
-bq query --nouse_legacy_sql --parameter commitId:STRING:$SHORT_SHA \
-'
+bq query --use_legacy_sql=false --parameter=commitId:STRING:$SHORT_SHA '
 SELECT 
 " Over the previous commit, the CPU usage improved by "  || ROUND(cpu_improvement,2) || " CPU-Seconds " 
 " and the memory usage improved by "  || ROUND(mem_improvement,2) || " CPU-Seconds " 
   FROM `greenops-demo-env.greenpipeline_metering.commit_usage_impact` 
-WHERE commitId = "@commitId"
+WHERE commitId = @commitId
+
 '
-
-
  
