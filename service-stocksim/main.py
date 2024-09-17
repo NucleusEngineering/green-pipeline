9c8fbc4aeaee7a55258d89eeed99d11a1fa84b8c
midietz <midietz@google.com>
2024-09-17 13:37:24 +0000

    update stocksim

--- service-stocksim/main.py
+++ service-stocksim/main.py
@@ -12,23 +12,29 @@ def simulate_stock_price(methods=['GET', 'POST']):
     stock_symbol = request.args.get('stock')
     sim_iterations  = request.args.get('iterations', default=1000) 
     
-    if stock_symbol is None or stock_symbol == "":
-       return Response("URL parameter stock must be provided", status=200)
+    # if stock_symbol is None or stock_symbol == "":
+    #    return Response("URL parameter stock must be provided", status=200)
  
-    #call API to get stock information
-    url = "https://yfapi.net/v6/finance/quote"
-    querystring = {}
-    symbols = stock_symbol
-    for information in ["symbols"]:      
-        querystring[information] = eval(information)  
+    # #call API to get stock information
+    # url = "https://yfapi.net/v6/finance/quote"
+    # querystring = {}
+    # symbols = stock_symbol
+    # for information in ["symbols"]:      
+    #     querystring[information] = eval(information)  
  
-    headers = {
-    'x-api-key': "xKqFyxmTgdauCo4S1LRdD5adrSlyFK9J9if4VFfZ"
-    }
+    # headers = {
+    # 'x-api-key': "xKqFyxmTgdauCo4S1LRdD5adrSlyFK9J9if4VFfZ"
+    # }
     
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    response_data =  response.json()
-    stock_price = response_data['quoteResponse']['result'][0]['regularMarketPrice']
+    # response = requests.request("GET", url, headers=headers, params=querystring)
+    # response_data =  response.json() 
+    
+    if stock_symbol = "GOOG":
+        stock_price = 159.96
+    elif stock_symbol = "AAPL":
+        stock_price = 214.83
+    else
+        stock_price = 121.83
  
     #call the service-montecarlo-py to calculate the mean of the montecarlo iterations
     url_montecarlo = "http://montecarlo-svc:8080"
